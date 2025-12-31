bc9845eeb3c6cf9ad9acf66eca93b292f1795560
abhijeet-talmale <84791446+abhijeet-talmale@users.noreply.github.com>
2025-12-31 20:46:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://fissionfrontend45.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
